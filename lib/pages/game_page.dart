d1337c40b2b26a3992a39d40a2ea7974d2e526ec
user <shaxriyortursunaliyev17@gamil.com>
2021-12-13 09:07:13 +0500

    added true shoot

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -19,56 +19,15 @@ class _GamePageState extends State<GamePage> {
   double sizeW = 120.0;
   double sizeH = 120.0;
   double ra = 0;
-  double dartRanX =
-      0.7; //pow(-1, Random().nextInt(10)) * Random().nextDouble();
-  double dartRanY =
-      -0.75; //+ pow(-1, Random().nextInt(10)) * Random().nextDouble();
+  // int qadam = 10;
+
+  double dartRanX = ((Random().nextInt(5)) / 10) * pow(-1, Random().nextInt(10));
+  double dartRanY = ((Random().nextInt(5)+3) / 10) * (-1);
+
   int index = 1;
-  int point = 15;
-
-  void shoot() {
-    print("On tapped");
-    Timer.periodic(const Duration(microseconds: 100), (timer) {
-      setState(() {
-        // index++;
-        // dartY -= 0.005;
-        // dartX -= (dartX-dartRanX)/100;
-        // if(index == 3){
-        //    index = 1;
-        // }
-        dartX = dartRanX;
-        dartY = dartRanY;
-      });
-      // dartX = dartRanX;
-      // if(dartY <= dartRanY ){
-      //   timer.cancel();
-      // }
-    });
-    ra = sqrt(pow(((-0.4) - (dartRanY)), 2) + pow(((-0.06) - (dartRanX)), 2));
-    if (ra <= 0.085) {
-      point = 10;
-      scoreAddFunction(10);
-    } else if (ra > 0.085 && ra <= 0.17) {
-      point = 8;
-      scoreAddFunction(8);
-    } else if (ra > 0.17 && ra <= 0.27) {
-      point = 6;
-      scoreAddFunction(6);
-    } else if (ra > 0.27 && ra <= 0.38) {
-      point = 4;
-      scoreAddFunction(4);
-    } else if (ra > 0.38 && ra <= 0.48) {
-      point = 2;
-      scoreAddFunction(2);
-    } else if (ra > 0.48 && ra <= 0.69) {
-      point = 1;
-      scoreAddFunction(1);
-    } else {
-      point = 0;
-      scoreAddFunction(0);
-    }
-  }
-
+  int p = 15;
+  double rukaY = 1;
+  double x = 0;
   List list = [];
 
   void scoreAddFunction(int a) {
@@ -79,7 +38,7 @@ class _GamePageState extends State<GamePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    scoreAddFunction(point);
+    scoreAddFunction(p);
   }
 
   static TextStyle _textStyle(double size) {
@@ -91,6 +50,7 @@ class _GamePageState extends State<GamePage> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    x= (width)/height;
 
     return Scaffold(
         body: SafeArea(
@@ -110,19 +70,71 @@ class _GamePageState extends State<GamePage> {
                   width: 400,
                   child: Stack(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 70.0),
-                        child: Image(
-                          image: AssetImage('assets/images/darts_table.png'),
+                      Container(
+                        alignment: Alignment(0, -0.67),
+                        child: Stack(
+                          alignment: AlignmentDirectional(0, 0),
+                          children: [
+                            CircleAvatar(
+                              radius: 140,
+                              backgroundColor: Colors.black,
+                            ),
+                            Positioned(
+                                top: 2,
+                                child: Text(
+                                  "2",
+                                  style: TextStyle(fontSize: 25, color: Colors.white),
+                                )),
+                            CircleAvatar(
+                              radius: 110,
+                              backgroundColor: Colors.yellow,
+                            ),
+                            Positioned(
+                                top: 30,
+                                child: Text(
+                                  "4",
+                                  style: TextStyle(fontSize: 25, color: Colors.black),
+                                )),
+                            CircleAvatar(
+                              radius: 80,
+                              backgroundColor: Colors.red,
+                            ),
+                            Positioned(
+                                top: 60,
+                                child: Text(
+                                  "6",
+                                  style: TextStyle(fontSize: 25, color: Colors.white),
+                                )),
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundColor: Colors.yellow,
+                            ),
+                            Positioned(
+                                top: 93,
+                                child: Text(
+                                  "8",
+                                  style: TextStyle(fontSize: 25, color: Colors.black),
+                                )),
+                            CircleAvatar(
+                              radius: 20,
+                              backgroundColor: Colors.blue,
+                            ),
+                            Positioned(
+                                top: 127,
+                                child: Text(
+                                  "10",
+                                  style: TextStyle(fontSize: 25, color: Colors.white),
+                                )),
+                          ],
                         ),
                       ),
                       Container(
-                        alignment: Alignment(dartX, dartY),
-                        child: Container(
-                          child: Image.asset("assets/images/dart1.png"),
-                          width: 120,
-                          height: 120,
-                        ),
+                        //dartX*(1/x)
+                        alignment: Alignment((1/x)*dartX, dartY),
+                        child: CircleAvatar(
+                          radius: 5,
+                          backgroundColor: Colors.white,
+                        )
                       ),
                     ],
                   ),
@@ -327,15 +339,66 @@ class _GamePageState extends State<GamePage> {
     );
   }
 
-  void _showDialog() {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
+  bool stop=false;
 
-        );
-      }
-    );
+
+  void shoot() {
+       stop=true;
+    print("On tapped");
+    Timer.periodic(const Duration(microseconds: 100), (timer) {
+      setState(() {
+        dartX = (1/x)*((Random().nextInt(5)) / 10) * pow(-1, Random().nextInt(10));
+        dartY = ((Random().nextInt(5)+2) / 10) * (-1);
+
+      });
+      // dartX = dartRanX;
+      // if(dartY <= dartRanY ){
+      //   timer.cancel();
+      // }
+
+
+     if(stop){
+       ra = sqrt(pow(((0) - ((1/x)*((Random().nextInt(5)) / 10) * pow(-1, Random().nextInt(10)))), 2) + pow(((-0.39) - (((Random().nextInt(5)+3) / 10) * (-1))), 2));
+       if (ra <= 0.06) {
+         p = 10;
+         scoreAddFunction(10);
+       } else if (ra > 0.06 && ra <= 0.15) {
+         p = 8;
+         scoreAddFunction(8);
+       } else if (ra > 0.15 && ra <= 0.245) {
+         p = 6;
+         scoreAddFunction(6);
+       } else if (ra > 0.245 && ra <= 0.336) {
+         p = 4;
+         scoreAddFunction(4);
+       } else if (ra > 0.336 && ra <= 0.43) {
+         p = 2;
+         scoreAddFunction(2);
+       } else {
+         p = 0;
+         scoreAddFunction(0);
+       }
+
+     }
+      stop=false;
+      Future.delayed(Duration(seconds: 5));
+      setState(() {
+        dartX=0;
+        dartY=1;
+      });
+    });
+
+
+
+
+
   }
+
+  // Future<void> qayting() async {
+  //   if(qadam > 0){
+  //     dartX = 0;
+  //     dartY = 1;
+  //     qadam--;
+  //   }
+  // }
 }
